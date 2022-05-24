83d147c2476850e4bfc2ad8c22147f97cdc71c63
sergeiovs <mr.serz98@mail.ru>
2022-05-23 18:19:17 -0700

    123

---
+++ Myfirstproject/test1.java
@@ -0,0 +1,10 @@
+public class test1 {public static void main(String[] args) 
+
+{
+        String name = "Sergei";
+        int age = 24;
+        int height = 170;
+        System.out.printf("%s,%s,%s", name,age,height);
+}
+
+}
